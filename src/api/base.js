27ad43d152003c0ba9087f834b6511f0387acbef
zxtname <zxt_name@163.com>
2020-04-19 17:38:55 +0800

    '添加接口'

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = "/api/"; // 这里填入我们之前在代理中写好的那个。
-const baseUrl="./mock/";//静态的路由
+// const baseUrl = "/api/"; 这里设置webapi前缀地址
+const baseUrl="./mock/";//不想连接数据库则用静态数据
 
 const http = axios.create({
   baseURL: baseUrl
