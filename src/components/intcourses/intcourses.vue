a95d07ee81dd8e544226bddd65e9e2a28ce02664
zxtname <zxt_name@163.com>
2020-04-21 08:09:37 +0800

    '融合'

--- src/components/intcourses/intcourses.vue
+++ src/components/intcourses/intcourses.vue
@@ -2,18 +2,17 @@
 	<div class="intcourses">
 		<intnav></intnav>
 		<cards :dataList="dataList"></cards>
-		<tabPage :totalPage="totalPage" @changePage="changePage"></tabPage>
+		<tabPage :totalPage="coursesContent" @changePage="changePage"></tabPage>
 		<!-- <text></text> -->
 	</div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import intnav from '@/components/intcourses/intnav.vue';
 // import text from '@/components/intcourses/text.vue'
 import cards from '@/components/common/cards.vue';
 import tabPage from '@/components/common/tabPage.vue';
-import {GetCourseInfo} from 'api/intCourse'
+import coursesApi from '@/api/mock/home/index.js';
 
 export default {
 	components: {
@@ -26,87 +25,35 @@ export default {
 		return {
 			totalPage: 1,
 			currentPage: 1,
-			dataList: [],
-			query:{
-				pageNum:1,
-				field:"id"
-=======
-	import intnav from '@/components/intcourses/intnav.vue'
-	// import text from '@/components/intcourses/text.vue'
-	import cards from '@/components/common/cards.vue'
-	import tabPage from '@/components/common/tabPage.vue'
-	import coursesApi from '@/api/mock/home/index.js'
-
-	export default {
-		components: {
-			intnav,
-			cards,
-			tabPage,
-			// text,
-		},
-		data() {
-			return {
-				totalPage: 1,
-				currentPage: 1,
-				coursesContent: [],
->>>>>>> ea2fcf07979f2ca8cf63cdac0cebde03f75fcad8
-			}
+			coursesContent: []
 		};
 	},
+
 	methods: {
-		async GetData(){
-			const {data: res}= await GetCourseInfo(this.query)
-			console.log(res)			
-		},		
 		routerTo() {
 			this.$router.push({
 				name: 'intcourses',
 				query: {
+					...this.$route.query,
 					page: this.currentPage
 				}
 			});
 		},
-<<<<<<< HEAD
 		changePage(page) {
 			this.currentPage = page;
 			this.routerTo();
 		}
 	},
-	created() {
-		this.GetData();
-		// this.totalPage = Math.ceil(resTwo.data.count / resTwo.data.page_size);
-=======
-		methods: {
-		        routerTo() {
-		            this.$router.push({
-		                name: 'intcourses',
-		                query: {
-							...this.$route.query,
-		                    page: this.currentPage
-		                }
-		            })
-		        },
-		        changePage(page) {
-		            this.currentPage = page
-		            this.routerTo()
-		        }
-		    },
-		    async created () {
-				let resTwo = await coursesApi.getCoursesContent(this.$route.query)
-				this.coursesContent = resTwo.data		
-		        this.totalPage = Math.ceil(resTwo.data.count / resTwo.data.page_size)
-		    }
->>>>>>> ea2fcf07979f2ca8cf63cdac0cebde03f75fcad8
+	async created() {
+		let resTwo = await coursesApi.getCoursesContent(this.$route.query);
+		this.coursesContent = resTwo.data;
+		this.totalPage = Math.ceil(resTwo.data.count / resTwo.data.page_size);
 	}
 };
 </script>
 
-<<<<<<< HEAD
-<style scoped></style>
-=======
 <style scoped>
-	.intcourses{
-		min-height: 600px;
-	}
+.intcourses {
+	min-height: 600px;
+}
 </style>
->>>>>>> ea2fcf07979f2ca8cf63cdac0cebde03f75fcad8
