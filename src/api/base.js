7e1b62dd44165f3fb6ebbc457dadb70f3bf72dd8
zxtname <zxt_name@163.com>
2020-04-23 20:26:48 +0800

    “远程”

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-const baseUrl="./mock/";//静态的路由
+// const baseUrl="./mock/";//静态的路由
 
-// const baseUrl = "http://localhost:51534/api/"; //这里设置webapi前缀地址
+// const baseUrl = "http://localhost:51534/api/"; //这里设置测试webapi前缀地址
+const baseUrl="http://rdjdmy.natappfree.cc/api/";
 //const baseUrl="./mock/";//不想连接数据库则用静态数据
 
 const http = axios.create({
